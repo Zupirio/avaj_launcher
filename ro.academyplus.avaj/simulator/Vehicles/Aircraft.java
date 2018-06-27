a411fa4a0029860fce589fabceebf1ea5358b7b9
Asahel RANGARIRA <arangari@e4r13p4.wethinkcode.co.za>
2018-06-27 09:34:04 +0200

    1st steps

---
+++ ro.academyplus.avaj/simulator/Vehicles/Aircraft.java
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Aircraft.java                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arangari <arangari@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/26 11:50:42 by arangari          #+#    #+#             */
+/*   Updated: 2018/06/26 16:41:56 by arangari         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+package Vehicles;
+import com.sun.xml.internal.bind.v2.runtime.Coordinator;
+
+public class Aircraft
+{
+	protected
+			Long id;
+			String name;
+			Coordinator coordinator;
+	private
+			Long idCounterLong;
+
+}
---
+++ ro.academyplus.avaj/simulator/Vehicles/AircraftFactory.java
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   AircraftFactory.java                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arangari <arangari@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/26 11:41:07 by arangari          #+#    #+#             */
+/*   Updated: 2018/06/26 16:41:46 by arangari         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+package Vehicles;
+
+public class AircraftFactory
+{
+	AircraftFactory foo = new AircraftFactory();
+}
