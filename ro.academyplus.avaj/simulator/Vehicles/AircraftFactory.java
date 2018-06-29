448f80b01ca29e97a40a87d0d7697de075781dc9
Asahel RANGARIRA <arangari@e4r13p4.wethinkcode.co.za>
2018-06-29 15:33:42 +0200

    Declared vehicles in factory.

--- ro.academyplus.avaj/simulator/Vehicles/AircraftFactory.java
+++ ro.academyplus.avaj/simulator/Vehicles/AircraftFactory.java
@@ -6,16 +6,36 @@
 /*   By: arangari <arangari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/26 11:41:07 by arangari          #+#    #+#             */
-/*   Updated: 2018/06/27 16:28:10 by arangari         ###   ########.fr       */
+/*   Updated: 2018/06/29 15:30:00 by arangari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 package Vehicles;
 
+import javax.lang.model.util.ElementScanner6;
+import ro.academyplus.avaj.weather.Coordinates;
+
 public class AircraftFactory
 {
-	protected Aircraft(String type, String name, Coordinator coordinator, int logitude,  int latitude, int height)
+	public static Flyable Aircraft(String type, String name, int longitude,  int latitude, int height)
 	{
-		return Flyable;
+		Coordinates coordinates = new Coordinates(longitude, latitude, height);
+
+		if (type.equals("Helicopter"))
+		{
+			return new Helicopter(name, coordinates);
+		}
+		else if (type.equals("Jetplane"))
+		{
+			return new JetPlane(name, coordinates);
+		}
+		else if (type.equals("Baloon"))
+		{
+			return new Baloon(name, coordinates);
+		}
+		else
+		{
+			return null;
+		}
 	}
 }
