79f6cc215c864742d7f0ee898b463826748db6b3
Asahel RANGARIRA <arangari@e4r13p4.wethinkcode.co.za>
2018-07-02 17:36:33 +0200

    added option to randomly choose weather

--- ro.academyplus.avaj/weather/WeatherProvider.java
+++ ro.academyplus.avaj/weather/WeatherProvider.java
@@ -6,21 +6,28 @@
 /*   By: arangari <arangari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/26 17:12:37 by arangari          #+#    #+#             */
-/*   Updated: 2018/06/27 18:12:08 by arangari         ###   ########.fr       */
+/*   Updated: 2018/07/02 17:35:32 by arangari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+// package ro.academy.avaj.weather;
+
 public class WeatherProvider
 {
-	private WeatherProvider weatherProvider;
-	private string[] weather;
+	private static WeatherProvider weatherProvider = new WeatherProvider();
+	private static string[] weather = {"RAIN", "FOG", "SUN", "SNOW"};
 
 	private WeatherProvider()
 	{}
 
-	public WeatherProvider getProvider()
-	{}
+	public static WeatherProvider getProvider()
+	{
+		return weatherProvider;
+	}
 
 	public String getCurrentWeather(Coordinates coordinates)
-	{}
+	{
+		int	weatherOption = coordinates.getHeight() + coordinates.getLatitude() + coordinates.getLongitude();
+		return weather[weatherOption % 4];
+	}
 }
