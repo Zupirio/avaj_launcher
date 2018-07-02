17d7359004842aefc4c93bf4633be8614a7e1336
Asahel RANGARIRA <arangari@e4r13p4.wethinkcode.co.za>
2018-07-02 08:31:51 +0200

    class to handle weather and aircraft simulation

--- ro.academyplus.avaj/simulator/Vehicles/Aircraft.java
+++ ro.academyplus.avaj/simulator/Vehicles/Aircraft.java
@@ -6,7 +6,7 @@
 /*   By: arangari <arangari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/26 11:50:42 by arangari          #+#    #+#             */
-/*   Updated: 2018/06/27 18:10:14 by arangari         ###   ########.fr       */
+/*   Updated: 2018/07/02 08:30:40 by arangari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,22 @@ public class Aircraft
 	protected long id;
 	protected string name;
 	protected Coordinates coordinates;
-	private long idCounter;
+	private static long idCounter = 1;
 
 	protected Aircraft(String name, Coordinates coordinates)
-	{}
+	{
+		this.name = name;
+		this.coordinates = coordinates;
+		this.id = nextId();
+	}
 
 	private long nextId()
-	{}
+	{
+		Aircraft.idCounter += 1;
+		return (Aircraft.idCounter);
+	}
 }
+
+/*
+	This class handles the aircraft and weather simulation
+ */
