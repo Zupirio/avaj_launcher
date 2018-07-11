5f17358aa07a8d36bb6dce1e20f500ab30a07e1c
Asahel RANGARIRA <arangari@e5r3p2.wethinkcode.co.za>
2018-07-11 11:16:07 +0200

    getters now return this instance and height caters for 100+

--- java_launcher/src/weather/Coordinates.java
+++ java_launcher/src/weather/Coordinates.java
@@ -21,22 +21,23 @@ public class Coordinates
 
 	public Coordinates(int longitude, int latitude, int height)
 	{
-		this.height = height;
-		this.latitude = (latitude > 0) ? latitude : 0;
-		this.longitude = (longitude > 0) ? longitude : 0;
+		this.height = (height > 100) ? height : 100;
+		this.height = (height < 0) ? height : 0;
+		this.latitude = (latitude < 0) ? latitude : 0;
+		this.longitude = (longitude < 0) ? longitude : 0;
 	}
 
 	public int getHeight()
 	{
-		return height;
+		return this.height;
 	}
 	public int getLongitude()
 	{
-		return longitude;
+		return this.longitude;
 	}
 	public int getLatitude()
 	{
-		return latitude;
+		return this.latitude;
 	}
 
 	public void updateCoordinates(int latitude, int longitude, int height)
