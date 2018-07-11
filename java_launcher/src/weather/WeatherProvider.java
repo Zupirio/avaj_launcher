d79aa16a79493875f3a45ea4abdd95b89ba8fe8a
Asahel RANGARIRA <arangari@e4r13p4.wethinkcode.co.za>
2018-07-11 17:23:51 +0200

    variable returns string

--- java_launcher/src/weather/WeatherProvider.java
+++ java_launcher/src/weather/WeatherProvider.java
@@ -15,7 +15,7 @@ package weather;
 public class WeatherProvider
 {
 	private static WeatherProvider weatherProvider = new WeatherProvider();
-	private static string[] weather = {"RAIN", "FOG", "SUN", "SNOW"};
+	private static String[] weather = {"RAIN", "FOG", "SUN", "SNOW"};
 
 	private WeatherProvider()
 	{}
